75487171bc2db13720dec9098b734f6b804fb5a3
Abby Robins <abigailr2003@hotmail.ca>
2025-04-12 00:50:24 -0600

    Add dynamic icons to weather api
    Add icons that change according to the weather data.

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
+const temperature = document.getElementById('temperature');
+
 temperature.textContent = "Loading . . .";
 
 fetch("https://api.open-meteo.com/v1/forecast?latitude=0.7551&longitude=73.4352&hourly=temperature_2m&current=temperature_2m,is_day,snowfall,cloud_cover,wind_speed_10m,wind_gusts_10m,showers,rain,weather_code&timezone=auto&forecast_days=1")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.current.is_day);
+        console.log(data.current.weather_code);
+        console.log(data.current.temperature_2m);
 
-.then(response => response.json())
+        temperature.textContent = "Currently " + data.current.temperature_2m + "°C";
 
-.then(data => {
-    console.log(data);
-    console.log(data.current.is_day);
-    //1 for day, 0 for night
-    console.log(data.current.weather_code);
-    console.log(data.current.temperature_2m);
-    console.log(data.current.wind_speed_10m);
-    console.log(data.current.wind_gusts_10m);
-    console.log(data.current.cloud_cover);
+        updateWeatherIcon(data);
+    });
 
-    temperature.textContent = data.current.temperature_2m + "°C";
+function updateWeatherIcon(data) {
+    const isDay = data.current.is_day;
+    const weatherCode = data.current.weather_code;
+    let iconPath;
+    const weatherIcon = document.getElementById("weather-icon");
 
-});
+    if (isDay === 1) {
+        if (weatherCode >= 0 && weatherCode <= 1) {
+        iconPath = "./media/icons/weather/weather-day-sun.png";
+        } else if (weatherCode === 2) {
+            iconPath = "./media/icons/weather/weather-day-sun-cloudy.png"
+        } else if (weatherCode >= 3 && weatherCode <= 60) {
+            iconPath = "./media/icons/weather/weather-day-cloudy.png"
+        } else if (weatherCode >= 61 && weatherCode <= 62) {
+            iconPath = "./media/icons/weather/weather-day-light-rain.png"
+        } else if (weatherCode >= 63 && weatherCode <= 75) {
+            iconPath = "./media/icons/weather/weather-day-heavy-rain.png"
+        } else if (weatherCode >= 76 && weatherCode <= 80) {
+            iconPath = "./media/icons/weather/weather-day-light-rain.png"
+        } else if (weatherCode >= 81 && weatherCode <= 82) {
+            iconPath = "./media/icons/weather/weather-day-heavy-rain.png"
+        } 
+    } else if (isDay === 0) {
+        if (weatherCode >= 0 && weatherCode <= 1) {
+        iconPath = "./media/icons/weather/weather-night-sun.png";
+        } else if (weatherCode === 2) {
+            iconPath = "./media/icons/weather/weather-night-sun-cloudy.png"
+        } else if (weatherCode >= 3 && weatherCode <= 60) {
+            iconPath = "./media/icons/weather/weather-night-cloudy.png"
+        } else if (weatherCode >= 61 && weatherCode <= 62) {
+            iconPath = "./media/icons/weather/weather-night-light-rain.png"
+        } else if (weatherCode >= 63 && weatherCode <= 75) {
+            iconPath = "./media/icons/weather/weather-night-heavy-rain.png"
+        } else if (weatherCode >= 76 && weatherCode <= 80) {
+            iconPath = "./media/icons/weather/weather-night-light-rain.png"
+        } else if (weatherCode >= 81 && weatherCode <= 82) {
+            iconPath = "./media/icons/weather/weather-night-heavy-rain.png"
+        } 
+    }
 
-const currentTemperature = document.getElementById('temperature');
-const currentTime = document.getElementById('current-time');
-const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.src = iconPath;
+
+}
